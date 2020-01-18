de70f6ca40368e977bd0e18b6556b30bb6c19fd9
Jeff Clark <jeffclarktopeka@yahoo.com>
2020-01-17 20:50:06 -0600

    made the code look a bit cleaner with some captions so you know what each thing should do.

--- js/index.js
+++ js/index.js
@@ -41,82 +41,107 @@ const siteContent = {
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
 
+//Adds Services To NavBar.
 let servicesNav = document.getElementsByTagName("a").item(0);
 servicesNav.textContent = (siteContent["nav"]["nav-item-1"])
 
+//Adds Product To NavBar.
 let productNav = document.getElementsByTagName("a").item(1);
 productNav.textContent = (siteContent["nav"]["nav-item-2"])
 
+//Adds Vision To NavBar.
 let visionNav = document.getElementsByTagName("a").item(2);
 visionNav.textContent = (siteContent["nav"]["nav-item-3"])
 
+//Adds Features To NavBar.
 let featuresNav = document.getElementsByTagName("a").item(3);
 featuresNav.textContent = (siteContent["nav"]["nav-item-4"])
 
+//Adds About To NavBar.
 let aboutNav = document.getElementsByTagName("a").item(4);
 aboutNav.textContent = (siteContent["nav"]["nav-item-5"])
 
+//Adds Contact To NavBar.
 let contactNav = document.getElementsByTagName("a").item(5);
 contactNav.textContent = (siteContent["nav"]["nav-item-6"])
 
+//Adds the top image.
 let cta = document.getElementById("cta-img");
 cta.setAttribute('src', siteContent["cta"]["img-src"])
 
+//Adds Title.
 let awesome = document.getElementsByTagName("h1").item(0);
 awesome.textContent = (siteContent["cta"]["h1"])
 
+//Adds Get Started Button.
 let button = document.getElementsByTagName("button").item(0);
 button.textContent = (siteContent["cta"]["button"])
 
+//Adds Features Title.
 let features = document.getElementsByTagName("h4").item(0);
 features.textContent = (siteContent["main-content"]["features-h4"])
 
+//Adds Features Paragraph.
 let featuresP = document.getElementsByTagName("p").item(0);
 featuresP.textContent = (siteContent["main-content"]["features-content"])
 
-
+//Adds About Title.
 let about = document.getElementsByTagName("h4").item(1);
 about.textContent = (siteContent["main-content"]["about-h4"])
 
+//Adds About Paragraph.
 let aboutP = document.getElementsByTagName("p").item(1);
 aboutP.textContent = (siteContent["main-content"]["about-content"])
 
+//Adds middle image.
 let midImg = document.getElementById("middle-img");
 midImg.setAttribute('src', siteContent["main-content"]["middle-img-src"])
 
+//Adds Services Title.
 let services = document.getElementsByTagName("h4").item(2);
 services.textContent = (siteContent["main-content"]["services-h4"])
 
+//Adds Services Paragraph.
 let servicesP = document.getElementsByTagName("p").item(2);
 servicesP.textContent = (siteContent["main-content"]["services-content"])
 
+//Adds Product Title.
 let product = document.getElementsByTagName("h4").item(3);
 product.textContent = (siteContent["main-content"]["product-h4"])
 
+//Adds Product Paragraph.
 let productP = document.getElementsByTagName("p").item(3);
 productP.textContent = (siteContent["main-content"]["product-content"])
 
+//Adds Vision Title.
 let vision = document.getElementsByTagName("h4").item(4);
 vision.textContent = (siteContent["main-content"]["vision-h4"])
 
+//Adds Vision Paragraph.
 let visionP = document.getElementsByTagName("p").item(4);
 visionP.textContent = (siteContent["main-content"]["vision-content"])
 
+//Adds Contact Title.
 let contact = document.getElementsByTagName("h4").item(5);
 contact.textContent = (siteContent["contact"]["contact-h4"])
 
+//Adds Address to Contact.
 let contactP = document.getElementsByTagName("p").item(5);
 contactP.textContent = (siteContent["contact"]["address"])
 
+//Adds Phone to Contact.
 let contactP2 = document.getElementsByTagName("p").item(6);
 contactP2.textContent = (siteContent["contact"]["phone"])
 
+//Adds Email to Contact.
 let contactP3 = document.getElementsByTagName("p").item(7);
 contactP3.textContent = (siteContent["contact"]["email"])
 
+//Adds Footer to the page.
 let footer = document.getElementsByTagName("p").item(8);
 footer.textContent = (siteContent["footer"]["copyright"])
 
+//Changes each item on the NavBar to the color green.
 let navStyle = document.getElementsByTagName("a").item(0);
 navStyle.style.color = "green"
 
@@ -135,10 +160,12 @@ navStyle5.style.color = "green"
 let navStyle6 = document.getElementsByTagName("a").item(5);
 navStyle6.style.color = "green"
 
+//Adds Home to the beginning of the NavBar.
 let firstLink = document.getElementsByTagName("nav").item(0);
 let a = document.createElement("a");
 firstLink.prepend("Home")
 
+//Adds Store to the end of the NavBar.
 let lastLink = document.getElementsByTagName("nav").item(0);
 let a2 = document.createElement("a");
 lastLink.append("Store")
